e4cec1ef39b1b4151c96212869286843a223d799
wallace de paula <kollen22@outlook.com>
2021-04-13 23:05:05 -0300

    fix: erros-1

--- config/firebase/server.js
+++ config/firebase/server.js
@@ -9,11 +9,11 @@ export const firebase_DB = fireb.apps.length ?
         type: "service_account",
         auth_uri: "https://accounts.google.com/o/oauth2/auth",
         auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-        project_id: process.env.PROJECT_ID,
-        private_key_id: process.env.PRIVATE_KEY_ID,
-        private_key: process.env.PRIVATE_KEY,
-        client_email: process.env.CLIENT_EMAIL,
-        client_id: process.env.CLIENT_ID,
-        client_cert: process.env.CLIENT_CERT,
+        project_id: process.env.NEXT_PUBLIC_PROJECT_ID,
+        private_key_id: process.env.NEXT_PUBLIC_PRIVATE_KEY_ID,
+        private_key: process.env.NEXT_PUBLIC_PRIVATE_KEY,
+        client_email: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
+        client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+        client_cert: process.env.NEXT_PUBLIC_CLIENT_CERT,
     })
 });
